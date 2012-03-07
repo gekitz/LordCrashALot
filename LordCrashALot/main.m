526edd36e39614628e55bb1617a6db1b82885d61
Georg Kitz <georgkitz@gmail.com>
2012-03-07 18:13:04 +0100

    fixed bug with app delegate

--- LordCrashALot/main.m
+++ LordCrashALot/main.m
@@ -7,8 +7,19 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import "AppDelegate.h"
 
 int main(int argc, char *argv[])
 {
-    return NSApplicationMain(argc, (const char **)argv);
+    @autoreleasepool {
+        NSApplication * application = [NSApplication sharedApplication];
+        
+        AppDelegate * appDelegate = [[AppDelegate alloc] init];
+        
+        [application setDelegate:appDelegate];
+        [application run];
+    }
+    
+    
+    return EXIT_SUCCESS;
 }
